ddf6bc9e38920d4cd7118b71871d569f60a99442
Sean McPherson <seanmcp25@gmail.com>
2019-03-08 06:33:54 -0500

    Clean up boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import StylesContainer from '../styles/StylesContainer';
+
+function App(props) {
+    return (
+        <StylesContainer>
+            <div className="App">
+                Hello world!
+            </div>
+        </StylesContainer>
+    );
+}
+
+export default App;
